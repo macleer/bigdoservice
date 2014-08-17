101d10066dd99745be142b2d2d40a4a06b89dcb7
aixiaoxianggithub <aixiaoxiang2009@hotmail.com>
2014-08-17 16:44:02 +0800

    创建项目

---
+++ MobileService/BaseService.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ *服务基类,简单封装
+ *@author axx
+ *
+ */
+abstract class BaseService {
+	/**
+	 * 处理请求
+	 */
+	public function on_do() {
+		header ( 'Accept:application/json' );
+		header ( 'Content-Type:application/json' );
+		header ( 'Accept-Charset:UTF-8' );
+		$method = $_SERVER ['REQUEST_METHOD'];
+		$request = explode ( '/', substr ( @$_SERVER ['PATH_INFO'], 1 ) );
+		$request_func = '';
+		if (isset ( $request ) && $request !== null && $request !== '') {
+			$lg = count ( $request );
+			if ($lg > 0) {
+				$request_func = $request [0];
+			}
+		}
+		$result = '';
+		
+		switch ($method) {
+			case 'GET' :
+				if (! isset ( $request_func ) || $request_func === null || $request_func === '') {
+					$request_func = 'on_get';
+				}
+				if (isset ( $request_func ) && $request_func !== null && $request_func !== '') {
+					
+					$result = $this->{$request_func} ();
+				}
+				
+				// $result = $this->on_get ();
+				break;
+			case 'POST' :
+				if (! isset ( $request_func ) || $request_func === null || $request_func === '') {
+					$request_func = 'on_post';
+				}
+				$post_param = null;
+				$request_body = file_get_contents ( 'php://input' );
+				if (isset ( $request_body ) && $request_body !== null && $request_body !== '') {
+					$post_param = json_decode ( $request_body );
+				}
+				if (isset ( $request_func ) && $request_func !== null && $request_func !== '') {
+					$result = $this->{$request_func} ( $post_param );
+					echo $request_func;
+				}
+				// $result = $this->on_post ( $post_param );
+				break;
+		}
+		if (isset ( $result ) && $result !== null && $result !== '') {
+			
+			$result = json_encode ( $result );
+			// print_r ( json_last_error_msg () );
+			echo $result;
+		}
+	}
+	public abstract function on_get();
+	public abstract function on_post($param = null);
+}
+?>
---
+++ MobileService/HomeService.php
@@ -0,0 +1,43 @@
+<?php
+include_once (dirname ( __FILE__ ) . '/BaseService.php');
+class HomeService extends BaseService {
+	public function on_get() {
+	}
+	public function on_post($param = null) {
+	}
+	public function getTop1() {
+		$result = new StdClass ();
+		$result->title = '张林老师火爆视频';
+		$result->teacher = '张老师';
+		$result->desc = '张老师大师级的视频....';
+		$result->img = 'Video/Img/1.jpg';
+		$result->video = 'Video/Img/1.jpg';
+		$result->__status = 1;
+		$result->__statusMsg = '检索到1条数据';
+		return $result;
+	}
+	
+	public function getTop10() {
+		$result = new StdClass ();
+		$videos = array ();
+		for($i = 0; $i < 10; $i ++) {
+			$item = array ();
+			$item ['title'] = '张林老师火爆视频' . $i;
+			$item ['teacher'] = '张老师' . $i;
+			$item ['desc'] = '张老师' . $i . '大师级的视频....';
+			$item ['img'] = 'Video/Img/1.jpg';
+			$item ['video'] = 'Video/Img/1.jpg';
+			array_push ( $videos, $item );
+		}
+		$result->item = $videos;
+		$result->__status = 1;
+		$result->__statusMsg = '检索到10条数据';
+		return $result;
+	}
+    
+
+
+}
+$bll = new HomeService ();
+$bll->on_do ();
+?>
