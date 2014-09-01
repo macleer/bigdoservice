51166548ddc15ea108f0c18dadb764ad3d77e1e7
aixiaoxianggithub <aixiaoxiang2009@hotmail.com>
2014-09-01 23:00:46 +0800

    完善视频详细服务

--- MobileService/RelatedService.php
+++ MobileService/RelatedService.php
@@ -7,31 +7,42 @@ class RelatedService extends BaseService {
 	}
 	public function getVideo() {
 		// sleep(1*10);
-		$result = new StdClass ();
-		$videos = array ();
-		for($i = 0; $i < 50; $i ++) {
-			$item = array ();
-			$item ['idvideo'] = ($i + 1) . '0';
-			$item ['idtype'] = ($i + 1) . '0';
-			$item ['score'] = 10 * ($i + 1);
-			$item ['title'] = '张林老师火爆视频' . $i;
-			$item ['idteacher'] = '100';
-			$item ['teacher'] = '张老师' . $i;
-			$item ['taptitudes'] = '特牛B的老师，获得多项国际大奖.';
-			$item ['timg'] = '1.jpg';
-			$item ['desc'] = '张老师' . $i . '大师级的视频,大师级的视频,大师级的视频,大师级的视频,,大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频....';
-			$item ['paycount'] = $i + 10;
-			$item ['paytime'] = '00:' . $i . '0:00';
-			$item ['img'] = '1.jpg';
-			$item ['video'] = '1.mp4';
-			$item ['createtime'] = date ( "Y-m-d H:i:s" );
-			$item ['updatetime'] = date ( "Y-m-d H:i:s" );
-			array_push ( $videos, $item );
+		$idvideo = '';
+		if (array_key_exists ( 'idvideo', $_GET )) {
+			$idvideo = $_GET ['idvideo'];
 		}
-		$result->item = $videos;
-		$result->__status = 1;
-		$result->__stateInfo = '检索到50条数据';
-		$result->__result = 1;
+	
+		$result = new StdClass ();
+		$result->item = null;
+		$result->__status = 0;
+		$result->__stateInfo = '没有找到相关数据';
+		$result->__result = 0;
+		if (isset ( $idvideo ) && $idvideo !== null && $idvideo !== '') {
+			$videos = array ();
+			for($i = 0; $i < 50; $i ++) {
+				$item = array ();
+				$item ['idvideo'] = ($i + 1) . '0';
+				$item ['idtype'] = ($i + 1) . '0';
+				$item ['score'] = 10 * ($i + 1);
+				$item ['title'] = '[相关]张林老师火爆视频' . $i;
+				$item ['idteacher'] = '100';
+				$item ['teacher'] = '张老师' . $i;
+				$item ['taptitudes'] = '特牛B的老师，获得多项国际大奖.';
+				$item ['timg'] = '1.jpg';
+				$item ['desc'] = '张老师' . $i . '大师级的视频,大师级的视频,大师级的视频,大师级的视频,,大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频,大师级的视频....';
+				$item ['paycount'] = $i + 10;
+				$item ['paytime'] = '00:' . $i . '0:00';
+				$item ['img'] = '1.jpg';
+				$item ['video'] = '1.mp4';
+				$item ['createtime'] = date ( "Y-m-d H:i:s" );
+				$item ['updatetime'] = date ( "Y-m-d H:i:s" );
+				array_push ( $videos, $item );
+			}
+			$result->item = $videos;
+			$result->__status = 1;
+			$result->__stateInfo = '检索到50条数据';
+			$result->__result = 1;
+		}
 		return $result;
 	}
 }
